cae9f44c5639f09a977a42ac0986e2b051281ed9
Eric Montalvo <ericmontalvo44@gmail.com>
2026-07-19 13:55:09 -0600

    HCEEK 7z extractor method to be used in new project feature

--- Ark/Utils/Utils.cs
+++ Ark/Utils/Utils.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Ark.Models.Config;
+using SevenZip;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -10,6 +12,26 @@ namespace Ark.Utils
         public static bool isHeekValidatedDirectory(string path)
         {
             return Path.Exists($"{path}\\tags") && Path.Exists($"{path}\\data") && File.Exists($"{path}\\tool.exe");
+        }
+
+        public static bool extractHCEEK7z(Config configModel, string path, string projectName)
+        {
+            try
+            {
+                string file7z = $"{configModel.HEEKPath}\\{ConstantsGlobals.FOLDER_NAME}.7z";
+                Directory.CreateDirectory($"{path}\\{projectName}");
+                SevenZipExtractor.SetLibraryPath("7z.dll");
+                if(File.Exists(file7z))
+                    using (var extractor = new SevenZipExtractor(file7z))
+                    {
+                        extractor.ExtractArchive(Path.Combine(path, projectName));
+                    }
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
         }
     }
 }
